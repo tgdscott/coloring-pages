e0093de4d3116341b01e2c4555d46cc871ab2617
tgdscott <scott@scottgerhardt.com>
2026-06-22 15:11:00 -0700

    Restore uploaded Spinner component

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 export const Spinner: React.FC = () => {
   return (
-    <div className="relative w-16 h-16">
-      <div className="absolute inset-0 rounded-full border-4 border-indigo-100" />
-      <div className="absolute inset-0 rounded-full border-4 border-transparent border-t-indigo-600 animate-spin" />
+    <div className="flex flex-col items-center justify-center p-8 space-y-4">
+      <div className="relative w-20 h-20">
+        <div className="absolute top-0 left-0 w-full h-full border-4 border-indigo-200 rounded-full animate-ping opacity-25"></div>
+        <div className="absolute top-0 left-0 w-full h-full border-4 border-t-indigo-600 border-r-transparent border-b-indigo-600 border-l-transparent rounded-full animate-spin"></div>
+      </div>
+      <p className="text-indigo-600 font-semibold animate-pulse">Creating your masterpiece...</p>
     </div>
   );
 };
